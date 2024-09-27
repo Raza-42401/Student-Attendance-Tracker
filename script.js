7053f7680c347f75bea6e422c46210bfa19671d0
Raza-42401 <officialkashaafraza@gmail.com>
2024-09-27 10:04:37 -0700

    Problem solved

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ var no_of_students = Number(prompt("enter the number of students"));
 for(var i = 0;i < no_of_students;i++){
     var input_name = prompt(`Enter the name of student(${i + 1})`);
     var attendance = prompt(`Enter whether student ${input_name} Present or Absent`);
+
+    if (attendance === "Present") {
+        present_student.push(input_name);
+    } else if (attendance === "Absent") {
+        absent_students.push(input_name);
+    }
 }
-if (attendance === "Present") {
-    present_student.push(input_name);
-} else if (attendance === "Absent") {
-    absent_students.push(input_name);
-}
+
 var numberOfPresentStudents = present_student.length;
 var numberOfAbsentStudents = absent_students.length;
 
